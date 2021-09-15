6809b877d66c90f444bd0c8db076c8d2c47fa374
TLaw763 <diego.aqu@outlook.com>
2021-09-15 08:42:32 +0200

    Initial Commit

---
+++ SimpliFi/SimpliFiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpliFiApp.swift
+//  SimpliFi
+//
+//  Created by Diego on 2021/09/15.
+//
+
+import SwiftUI
+
+@main
+struct SimpliFiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
